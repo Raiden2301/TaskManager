eccd0ae3207495d8ba85a0cb97d4496763e2ffbc
Raiden2301 <mireabogdancristian@gmail.com>
2020-06-26 19:05:24 +0300

    HomeController

--- server/src/main/java/com/etti/taskManager/HomeController.java
+++ server/src/main/java/com/etti/taskManager/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = {"/employees/", "/projects/", "/tasks/"})
+	@RequestMapping(value = {"/employees/", "/projects/", "/tasks/", "/report", "/todo"})
 	public String index() {
 		return "index";
 	}
